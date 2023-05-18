9da0d6da8cc1c95696f4fbc70083c48d4c1ab043
somayehbd <somayehbd@gmail.com>
2023-05-18 12:41:34 +0200

    added new test

--- family-age-gap/functions.js
+++ family-age-gap/functions.js
@@ -2,7 +2,10 @@ const functions = {
     ageGap: (arr) => {
         let n1 = arr[0];
         let n2 = arr[1];
-        return [n1,n2,n2-n1]
+        if (n1 < n2)
+            return [n1, n2, n2 - n1]
+        else
+            return [n2, n1, n1 - n2]
     }
 }
 
--- family-age-gap/functions.test.js
+++ family-age-gap/functions.test.js
@@ -10,4 +10,14 @@ describe('method: ageGap', () => {
         const result = functions.ageGap([10, 40]);
         expect(result).toEqual([10, 40, 30])
     });
+
+    it('should return[20,20,0] when i supply [20,20]', () => {
+        const result = functions.ageGap([20,20]);
+        expect(result).toEqual([20,20,0])
+    });
+
+    it('should return[20,40,20] when i supply [40,20]', () => {
+        const result = functions.ageGap([40,20]);
+        expect(result).toEqual([20,40,20])
+    });
 });
